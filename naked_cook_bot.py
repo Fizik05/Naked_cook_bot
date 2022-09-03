0c396ff9551259b3fba3b63d8891f8e7a3ab387b
shkray <cutaco.business@gmail.com>
2022-09-04 01:26:08 +0300

    Update naked_cook_bot.py

--- naked_cook_bot.py
+++ naked_cook_bot.py
@@ -2,15 +2,10 @@ import os
 
 import dotenv
 import requests
-
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import (Updater,
                           Filters,
-<<<<<<< Updated upstream
                           MessageHandler,
-=======
-                          MessageFilter,
->>>>>>> Stashed changes
                           CommandHandler)
 
 
@@ -20,48 +15,45 @@ token = os.getenv("TOKEN")
 updater = Updater(token=token)
 URL = ""
 
-<<<<<<< Updated upstream
+
+buttons = ReplyKeyboardMarkup([['Завтрак', 'Обед', 'Ужин']])
+reply_markup = buttons
+
+
+def for_errors(update, context):
+    updater.start_polling()
+    updater.idle()
+
 
 def wake_up(update, context):
     chat = update.effective_chat
+    
     if chat.last_name is None:
         context.bot.send_message(
             chat_id=chat.id,
-            text="Привет {}".format(chat.first_name)
+            text="Привет {}".format(chat.first_name),
         )
     else:
         context.bot.send_message(
             chat_id=chat.id,
-            text="Привет {} {}".format(chat.first_name, chat.last_name)
+            text="Привет {} {}".format(chat.first_name, chat.last_name),
         )
 
 
-def for_errors(update, context):
-    pass
+def TextHandler(update, context):
+    chat = update.effective_chat
+    if 'Завтрак' in chat or 'Обед' in chat or 'Ужин' in chat:
+        context.bot.send_message(
+            chat_id=chat.id,
+            text='Рецептов нету, но есть фотки котиков :)'
+        )
+        newrecipe()
+    else:
+        for_errors()
 
-    updater.start_polling()
-    updater.idle()
 
-
-updater.dispatcher.add_handler(CommandHandler("start", wake_up))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, for_errors))
+updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, TextHandler))
 updater.start_polling()
 updater.idle()
-=======
-def wake_up(update, context):
-    chat = update.effective_chat
 
-    context.bot.send_message(
-        chat_id=chat.id,
-        text="Hi, my name is Naked Cook",
-    )
-
-
-def main():
-    updater.dispatcher.add_handler(CommandHandler("start", wake_up))
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == "__main__":
-    main()
->>>>>>> Stashed changes
